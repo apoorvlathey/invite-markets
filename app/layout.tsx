8ea1271a26e9c8ed68ed8b46ff19548ec7bcb507
apoorvlathey <latheyapoorv@gmail.com>
2025-12-26 16:05:21 +0400

    fix: use aliased imports everywhere

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "@/app/globals.css";
 import { Providers } from "@/app/providers";
 import Image from "next/image";
 import Link from "next/link";
